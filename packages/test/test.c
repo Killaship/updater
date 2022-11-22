c0a4728d851e8b1ee03635f1ae084a36a58bb289
William Batts III <69988679+Killaship@users.noreply.github.com>
2022-11-22 17:29:58 -0500

    Update test.c

--- packages/test/test.c
+++ packages/test/test.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-const char *name = "Bob";
+const char *name = "Finn"; // used to be bob, this is the test
 
 int main() {
 	printf("Hello, %s%s", name, " the human!\n");
