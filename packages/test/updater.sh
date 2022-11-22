01750adc8ca14179df57c1229999c614a2d77f75
William Batts III <69988679+Killaship@users.noreply.github.com>
2022-11-22 18:48:57 -0500

    Update updater.sh

--- packages/test/updater.sh
+++ packages/test/updater.sh
@@ -1,6 +1,9 @@
 wget -O    new.json   https://raw.githubusercontent.com/Killaship/updater/dev/packages/test/test.json
 wget -O    diff.c      https://raw.githubusercontent.com/Killaship/updater/dev/packages/test/test.c
 
+# TODO:
+# Add functionality to update the updater script, and print out details from the package json.
+
 oldhash=($(md5sum "test.c"))
 echo $oldhash
 newhash=($(md5sum "diff.c"))
